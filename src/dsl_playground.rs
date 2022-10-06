65fab82b36fb8847e2208ac4993e709383ae1d17
Grenier Celestin <celestin.grenier@hotmail.com>
2022-10-06 16:47:56 +0200

    from/into technology

--- src/dsl_playground.rs
+++ src/dsl_playground.rs
@@ -4,6 +4,21 @@ use crate::engine::{Apply, Array, Function, Type, Typed, Value};
 
 use dsl_macro_lib::val;
 
+macro_rules! sure {
+    (let $id:ident = ($ty:ident)$vl:expr; $do:expr) => {
+        match $vl {
+            Value::$ty($id) => $do,
+            _ => unreachable!(),
+        }
+    };
+    (($ty:ident)$vl:ident) => {
+        match $vl {
+            Value::$ty(v) => v,
+            _ => unreachable!(),
+        }
+    };
+}
+
 macro_rules! named_val {
     ($name:ident :: $($t:tt)->+ = $def:expr) => {
         ( stringify!($name).to_string()
@@ -13,11 +28,31 @@ macro_rules! named_val {
     };
 }
 
+impl From<f32> for Value {
+    fn from(_: f32) -> Self {
+        todo!()
+    }
+}
+
+impl From<Value> for f32 {
+    fn from(_: Value) -> Self {
+        todo!()
+    }
+}
+
 fn _crap() -> Vec<(String, String, Value)> {
+    let r: f32 = 0.0;
+    let v: Value = Value::from(r);
+    let w: f32 = v.into();
+
     vec![
 
     named_val!(add :: Num -> Num -> Num = |a, b| a + b),
-    named_val!(sum :: [Num] -> Num = |a: Array| 0.0),
+    named_val!(sum :: [Num] -> Num = |a: Array| {
+        a.items
+            .iter()
+            .fold(0.0, |acc, cur| acc + sure!((Num)cur))
+    }),
 
     // named_val!(id :: a -> a = |a| a),
 
