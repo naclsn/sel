e8cfc9d162255659a98d28d0c95e1b83bc8faa7c
Grenier Celestin <celestin.grenier@hotmail.com>
2022-10-20 13:48:06 +0200

    lexer

--- lib/src/parser.cpp
+++ lib/src/parser.cpp
@@ -1,6 +1,8 @@
-#include <ostream>
-#include <istream>
-#include <string>
+#include <iterator>
+
+// ZZZ: some w is ip
+#include <iostream>
+#include <sstream>
 
 #include "sel/parser.hpp"
 #include "sel/visitors.hpp"
@@ -25,32 +27,91 @@ namespace sel {
       COMMA,
     } type;
     union {
-      std::string name;
+      std::string* name;
       double num;
-      std::string str;
+      std::string* str;
       char chr;
-    } as;
+    } as = {0};
+    Token() {}
+    Token(Token const& t) {
+      switch (type = t.type) {
+        case Token::Type::NAME:    as.name = new std::string(*t.as.name); break;
+        case Token::Type::LIT_STR: as.str = new std::string(*t.as.str);   break;
+        default: ;
+      }
+      loc = t.loc;
+    }
+    ~Token() {
+      switch (type) {
+        case Token::Type::NAME:    delete as.name; as.name = nullptr; break;
+        case Token::Type::LIT_STR: delete as.str;  as.str = nullptr;  break;
+        default: ;
+      }
+    }
   };
 
   // internal
+  std::ostream& operator<<(std::ostream& out, Token const& t) {
+    out << "Token { .type=";
+    switch (t.type) {
+      case Token::Type::NAME:          out << "NAME";          break;
+      case Token::Type::LIT_NUM:       out << "LIT_NUM";       break;
+      case Token::Type::LIT_STR:       out << "LIT_STR";       break;
+      case Token::Type::LIT_LST_OPEN:  out << "LIT_LST_OPEN";  break;
+      case Token::Type::LIT_LST_CLOSE: out << "LIT_LST_CLOSE"; break;
+      case Token::Type::UN_OP:         out << "UN_OP";         break;
+      case Token::Type::BIN_OP:        out << "BIN_OP";        break;
+      case Token::Type::SUB_OPEN:      out << "SUB_OPEN";      break;
+      case Token::Type::SUB_CLOSE:     out << "SUB_CLOSE";     break;
+      case Token::Type::COMMA:         out << "COMMA";         break;
+    }
+    out << ", ";
+    switch (t.type) {
+      case Token::Type::NAME:
+        out << ".name=" << (t.as.name ? *t.as.name : "-nil-");
+        break;
+
+      case Token::Type::LIT_NUM:
+        out << ".num=" << t.as.num;
+        break;
+
+      case Token::Type::LIT_STR:
+        out << ".str=\"" << (t.as.name ? *t.as.str : "-nil-") << '"';
+        break;
+
+      case Token::Type::LIT_LST_OPEN:
+      case Token::Type::LIT_LST_CLOSE:
+      case Token::Type::UN_OP:
+      case Token::Type::BIN_OP:
+      case Token::Type::SUB_OPEN:
+      case Token::Type::SUB_CLOSE:
+      case Token::Type::COMMA:
+        out << ".chr='" << t.as.chr << "'";
+        break;
+    }
+    return out << ", loc=" << t.loc << " }";
+  }
+
+  // internal
   std::istream& operator>>(std::istream& in, Token& t) {
-    char c = in.get();
+    t.loc = in.tellg();
+    char c = in.peek();
     // if (in.eof()) return in;
     switch (c) {
       // case -1: // trait::eof()
 
-      case ',': t.type = Token::Type::COMMA;         t.as.chr = c; break;
+      case ',': t.type = Token::Type::COMMA;         t.as.chr = in.get(); break;
 
-      case '[': t.type = Token::Type::SUB_OPEN;      t.as.chr = c; break;
-      case ']': t.type = Token::Type::SUB_CLOSE;     t.as.chr = c; break;
+      case '[': t.type = Token::Type::SUB_OPEN;      t.as.chr = in.get(); break;
+      case ']': t.type = Token::Type::SUB_CLOSE;     t.as.chr = in.get(); break;
 
-      case '{': t.type = Token::Type::LIT_LST_OPEN;  t.as.chr = c; break;
-      case '}': t.type = Token::Type::LIT_LST_CLOSE; t.as.chr = c; break;
+      case '{': t.type = Token::Type::LIT_LST_OPEN;  t.as.chr = in.get(); break;
+      case '}': t.type = Token::Type::LIT_LST_CLOSE; t.as.chr = in.get(); break;
 
       case '@':
       case '%':
         t.type = Token::Type::UN_OP;
-        t.as.chr = c;
+        t.as.chr = in.get();
         break;
 
       case '+':
@@ -58,20 +119,22 @@ namespace sel {
       case '.':
       case '/':
         t.type = Token::Type::BIN_OP;
-        t.as.chr = c;
+        t.as.chr = in.get();
         break;
 
       case ':':
         t.type = Token::Type::LIT_STR;
-        t.as.str = c;
-        while (!in.eof()) {
+        t.as.str = new std::string();
+        in.ignore(1);
+        do {
           c = in.get();
           if (':' == c) {
             if (':' != in.peek()) break;
             in.ignore(1);
           }
-          t.as.str.push_back(c);
-        }
+          t.as.str->push_back(c);
+        } while (!in.eof());
+        // if (in.eof()) ..
         break;
 
       default:
@@ -83,15 +146,28 @@ namespace sel {
 
         if ('a' <= c && c <= 'z') {
           t.type = Token::Type::NAME;
-          t.as.name = c;
-          while (isalpha(in.peek()))
-            t.as.name.push_back(in.get());
+          t.as.name = new std::string();
+          do {
+            t.as.name->push_back(in.get());
+          } while (isalpha(in.peek()));
           break;
         }
     }
 
     while (!in.eof() && isspace(in.peek())) in.get();
     return in;
+  }
+
+  // ZZZ: wip
+  void parseApplication() {
+    std::istringstream iss("split : :, map +1, join ::::");
+
+    std::istream_iterator<Token> isi(iss);
+    std::istream_iterator<Token> eos;
+
+    std::cerr << "parsing '" << iss.str() << "'\n";
+    while (eos != isi)
+      std::cerr << "  read token: " << *isi++ << std::endl;
   }
 
   std::ostream& operator<<(std::ostream& out, Application const& app) {
