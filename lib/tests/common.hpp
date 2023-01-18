3d11b5ab8b933a27e862a904a6554a3d8b103726
Grenier Celestin <celestin.grenier@hotmail.com>
2023-01-18 10:01:32 +0100

    codegen: thats not great
    .. made it work by having a `~/.local/bin/llvm-config-64`

--- lib/tests/common.hpp
+++ lib/tests/common.hpp
@@ -13,6 +13,7 @@ using namespace sel;
 #define GREEN "\e[32m"
 #define RESET "\e[m"
 
+#undef fail
 #define fail(__msg) do {                 \
   cerr                                   \
     << "failed with:\n"                  \
@@ -21,6 +22,7 @@ using namespace sel;
   exit(EXIT_FAILURE);                    \
 } while (0)
 
+#undef assert
 #define assert(__expr, __msg) do {       \
   if (__expr) break;                     \
   cerr                                   \
@@ -31,6 +33,7 @@ using namespace sel;
   exit(EXIT_FAILURE);                    \
 } while (0)
 
+#undef asssert_eq
 #define assert_eq(__should, __have) do {                      \
   auto _should = (__should);                                  \
   auto _have = (__have);                                      \
@@ -44,11 +47,13 @@ using namespace sel;
   exit(EXIT_FAILURE);                                         \
 } while (0)
 
+#undef asssert_cmp
 #define assert_cmp assert_eq
 // #define assert_cmp(__should, __have) _assert_cmp(#__should, #__have, __should, __have)
 // inline void _assert_cmp(char const* sshould, char const* shave, string should, string have) {
 // }
 
+#undef TEST
 #define TEST(__function)  \
   void __##__function();  \
   int main() {            \
